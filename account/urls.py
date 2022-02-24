2db187c53c99a97e9ffb1f391521b44aa05714e6
rezacho <reza.chookian@gmail.com>
2022-02-24 21:08:37 +0330

    create RegisterView and UserRegistrationForm in 'account' app

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.RgisterView.as_view(), name='user_register')
+    path('register', views.RegisterView.as_view(), name='user_register')
 ]
