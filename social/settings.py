a885193c4cc757406451ebeb248a78dd0c2cce51
rezacho <reza.chookian@gmail.com>
2022-02-24 18:14:35 +0330

    create url "home"

--- social/settings.py
+++ social/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
+    # My Apps
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
