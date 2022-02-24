c1977bd16039753f265738be5ca08c18a88312ef
rezacho <reza.chookian@gmail.com>
2022-02-24 21:13:10 +0330

    show UserRegistrationForm in register.html

--- account/views.py
+++ account/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import UserRegistrationForm
+
 # Create your views here.
 
 
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'account/register.html')
+        form = UserRegistrationForm()
+        return render(request, 'account/register.html', context={'form':form})
 
     def post(self, request):
         pass
