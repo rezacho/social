802d1725d80ec5eb5adb2e892efab81ece0d836c
rezacho <reza.chookian@gmail.com>
2022-02-25 10:50:02 +0330

    complete RegisterView code

--- account/views.py
+++ account/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 
 class RegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = 'account/register.html'
+
     def get(self, request):
-        form = UserRegistrationForm()
-        return render(request, 'account/register.html', context={'form':form})
+        form = self.form_class
+        return render(request, self.template_name, context={'form': form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password'])
+            messages.success(request, 'Registration complete successfully', extra_tags='success')
+            return redirect('home:home')
+        return render(request, self.template_name, context={'form': form})
