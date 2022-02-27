8c38e9ec8da3c3b692782875e6330bef444622b8
rezacho <reza.chookian@gmail.com>
2022-02-27 20:11:38 +0330

    Show posts in home page and post detail

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from home.models import Post
 
 # Create your views here.
 
