6b7ffb040d8a287d72bb48d6d48546a5f2f89ded
rezacho <reza.chookian@gmail.com>
2022-02-27 20:20:12 +0330

    Show each user posts in profile

--- account/views.py
+++ account/views.py
@@ -69,4 +69,5 @@ class UserLogoutView(LoginRequiredMixin, View):
 class UserProfileView(LoginRequiredMixin, View):
     def get(self, request, user_id):
         user = User.objects.get(pk=user_id)
-        return render(request, 'account/profile.html', {'user': user})
+        posts = Post.objects.filter(user=user)
+        return render(request, 'account/profile.html', {'user': user, 'posts': posts})
