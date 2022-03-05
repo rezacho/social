7b1e4af8522c29332f3afce974e2e07155769076
rezacho <reza.chookian@gmail.com>
2022-03-05 13:51:49 +0330

    PostUpdateView (def post, def setup), some minor improvement

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.utils.text import slugify
 from django.contrib import messages
+from django.views import View
 from .forms import PostUpdateForm
+from .models import Post
 
 # Create your views here.
 
@@ -34,17 +35,28 @@ class PostDeleteView(LoginRequiredMixin, View):
 class PostUpdateView(LoginRequiredMixin, View):
     form_class = PostUpdateForm
 
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        return super().setup(request, *args, **kwargs)
+
     def dispatch(self, request, *args, **kwargs):
-        post = Post.objects.get(pk=kwargs['post_id'])
+        post = self.post_instance
         if not post.user.id == request.user.id:
             messages.error(request, 'You cant update this post', extra_tags='danger')
             return redirect('home:home')
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
         form = self.form_class(instance=post)
         return render(request, 'home/update.html', context={'form': form})
 
-    def post(self, request, post_id):
-        pass
+    def post(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.save()
+            messages.success(request, 'Post updated successfully', extra_tags='success')
+            return redirect('home:post_detail', post.id, post.slug)
