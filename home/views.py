2ce66c52789192a97558cc63de002bb0ab92d088
rezacho <reza.chookian@gmail.com>
2022-03-06 14:02:00 +0330

    class PostCreateView (class, functions, html and url)

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.utils.text import slugify
 from django.contrib import messages
 from django.views import View
-from .forms import PostUpdateForm
+from .forms import PostCreateUpdateForm
 from .models import Post
 
 # Create your views here.
@@ -33,7 +33,7 @@ class PostDeleteView(LoginRequiredMixin, View):
 
 
 class PostUpdateView(LoginRequiredMixin, View):
-    form_class = PostUpdateForm
+    form_class = PostCreateUpdateForm
 
     def setup(self, request, *args, **kwargs):
         self.post_instance = Post.objects.get(pk=kwargs['post_id'])
@@ -60,3 +60,22 @@ class PostUpdateView(LoginRequiredMixin, View):
             new_post.save()
             messages.success(request, 'Post updated successfully', extra_tags='success')
             return redirect('home:post_detail', post.id, post.slug)
+
+
+class PostCreateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        return render(request, 'home/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.user = request.user
+            new_post.save()
+            messages.success(request, 'New post created successfully', extra_tags='success')
+            return redirect('home:post_detail', new_post.id, new_post.slug)
+
