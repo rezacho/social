b24fd432a067c1efa738893d72d66944d5bda171
rezacho <reza.chookian@gmail.com>
2022-03-08 23:26:41 +0330

    UserPasswordResetConfirmView

--- account/urls.py
+++ account/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('profile/<int:user_id>', views.UserProfileView.as_view(), name='user_profile'),
     path('reset/', views.UserPasswordResetView.as_view(), name='reset_password'),
     path('reset/dine/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('conform/<uidb64>/<token>', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
