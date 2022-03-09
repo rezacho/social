e2570d1e3eb77c665837021bca38d5a8e7ba5114
rezacho <reza.chookian@gmail.com>
2022-03-09 19:49:07 +0330

    some minor improve

--- home/models.py
+++ home/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     slug = models.SlugField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     class Meta:
         ordering = ['-created']
