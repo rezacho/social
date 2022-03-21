90c2596719eb228e9dca504d8039d4aac6f9b8ec
rezacho <reza.chookian@gmail.com>
2022-03-21 11:16:29 +0330

    create 'Comment' model and register to admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
@@ -11,3 +11,9 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('updated',)
     prepopulated_fields = {'slug': ('body',)}
     raw_id_fields = ('user',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created', 'is_reply')
+    rew_id_fields = ('user', 'post', 'reply')
